74130ff9bc294f24e71423dde49acc50d9d92b4a
garv-aga <garvagarwal2003@gmail.com>
2024-01-19 02:05:35 +0530

    added poa

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,10 +1,9 @@
- //@ts-ignore
+//@ts-ignore
 import { require } from "@hyperoracle/zkgraph-lib";
 import { Bytes, Block, Event } from "@hyperoracle/zkgraph-lib";
 
-let addr = Bytes.fromHexString('0x9D8e38b52F08FD7b0fc5C04460CdFC3AC30ce7bf');
-let esig_start = Bytes.fromHexString("0xdbac898e65d6e2eb500e4a2721266f81bbe66f2b88f851d052acf9cbe5828d38");
-let esig_end = Bytes.fromHexString("0x92556804021c81436e5a9adb2ccb99b74088418f5ee423d28553ace96b6fe7bd");
+let addr = Bytes.fromHexString('0x7169D38820dfd117C3FA1f22a697dBA58d90BA06');
+let esig_approval = Bytes.fromHexString("0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925");
 
 export function handleBlocks(blocks: Block[]): Bytes {
   // init output state
@@ -19,16 +18,15 @@ export function handleBlocks(blocks: Block[]): Bytes {
 
   // #3 also can access (matched) events of a given account address & a given esig  (should present in yaml first).
   // a subset of 'eventsByAcct'
-  let eventsByAcctEsigStart: Event[] = blocks[0].account(addr).eventsByEsig(esig_start)
-  let eventsByAcctEsigEnd: Event[] = blocks[0].account(addr).eventsByEsig(esig_end)
+  let eventsByAcctEsig: Event[] = blocks[0].account(addr).eventsByEsig(esig_approval)
 
   // require match event count > 0
-  require(eventsByAcctEsigStart.length > 0 && eventsByAcctEsigEnd.length > 0)
+  require(eventsByAcctEsig.length > 0)
 
   // this 2 way to access event are equal effects, alway true when there's only 1 event matched in the block (e.g. block# 2279547 on sepolia).
   require(
     events[0].data == eventsByAcct[0].data 
-    && events[0].data == eventsByAcctEsigStart[0].data && events[0].data == eventsByAcctEsigEnd[0].data
+    && events[0].data == eventsByAcctEsig[0].data
   );
 
   // set state to the address of the 1st (matched) event, demo purpose only.
